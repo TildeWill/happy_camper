24103d4a2cdfa4681bde6ab103af3bfbb7535ab4
Will Read <will.read@gmail.com>
2024-03-12 22:54:03 -0700

    Fixes the full build by removing some KiCAD 7 properties from the rat_bite.js

--- input/footprints/rat_bite.js
+++ input/footprints/rat_bite.js
@@ -6,17 +6,10 @@ module.exports = {
         const footprint = `
     (module "zzkeeb:Hole_Breakaway-Tabs" (layer "F.Cu")
         ${p.at /* parametric position */}
+        (fp_text reference "${p.ref}" (at 0 0) (layer F.SilkS) ${p.ref_hide} (effects (font (size 1.27 1.27) (thickness 0.15))))
 
-        (property "ki_description" "Mounting Hole without connection")
-        (property "ki_keywords" "mounting hole")
-        (path "/2b6bec09-e8d7-4a05-b994-6a0029410ea5")
         (attr virtual)
-        (fp_text reference "BR1" (at 0 -1.016 90) (layer "F.Fab") hide
-            (effects (font (size 1 1) (thickness 0.15)))
-        )
-        (fp_text value "MountingHole" (at -0.05 0.75 90) (layer "F.Fab") hide
-            (effects (font (size 1 1) (thickness 0.15)))
-        )
+        
         (fp_line (start -3 -0.2) (end 3 -0.2) (width 0.12) (layer "Dwgs.User") )
         (fp_line (start -3 0.2) (end 3 0.2) (width 0.12) (layer "Dwgs.User") )
         (pad "" np_thru_hole circle (at -2.375 0 90) (size 0.3 0.3) (drill 0.3) (layers "*.Cu" "*.Mask") )
@@ -28,8 +21,7 @@ module.exports = {
         (pad "" np_thru_hole circle (at 1.1875 0 90) (size 0.3 0.3) (drill 0.3) (layers "*.Cu" "*.Mask") )
         (pad "" np_thru_hole circle (at 1.78125 0 90) (size 0.3 0.3) (drill 0.3) (layers "*.Cu" "*.Mask") )
         (pad "" np_thru_hole circle (at 2.375 0 90) (size 0.3 0.3) (drill 0.3) (layers "*.Cu" "*.Mask") )
-  )
-    
+    )
     `
         return footprint;
     }
