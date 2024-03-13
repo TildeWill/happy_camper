561ada9485ecbd9e536d88da751fe41c1dc3151c
Will Read <will.read@gmail.com>
2024-03-12 23:13:02 -0700

    Lessens the module hole to allow greater use of PCB back side

--- input/footprints/shit.js
+++ input/footprints/shit.js
@@ -24,15 +24,14 @@ module.exports = {
       (fp_text reference ${p.ref} (at -1.037771 -10.340352) (layer "Dwgs.User") hide
         (effects (font (size 1 1) (thickness 0.15)))
       )
-      (fp_text value "Connector_VIK-keyboard" (at 0 -12) (layer "F.Fab") hide
+      (fp_text value "Connector_SHIT-keyboard" (at 0 -12) (layer "F.Fab") hide
         (effects (font (size 1 1) (thickness 0.15)))
       )
       (fp_circle (center -6.5 15.6) (end -6.5 15.4) (width 0.1) (layer "B.SilkS") )
       (fp_circle (center -6.5 15.6) (end -6.5 15.85) (width 0.1) (layer "F.SilkS") )
       (fp_circle (center 0 0) (end 23 0) (width 0.1) (layer "F.SilkS") )
       (fp_circle (center 0 0) (end 27.5 0) (width 0.1) (layer "F.SilkS") )
-      (fp_line (start -17 14) (end 17 14) (width 0.1) (layer "Edge.Cuts") )
-      (fp_arc (start 0 0) (end 17 14) (angle -258.9) (width 0.1) (layer "Edge.Cuts") )
+      (fp_circle (center 0 8) (end 0 4) (width 0.05) (layer "Edge.Cuts"))
       (pad "1" thru_hole circle (at -5.5 17 127) (size 1.7 1.7) (drill 0.9) (layers "*.Cu" "*.Mask")
         ${p.V3V.str} )
       (pad "2" thru_hole circle (at -5.5 19.2 127) (size 1.7 1.7) (drill 0.9) (layers "*.Cu" "*.Mask")
